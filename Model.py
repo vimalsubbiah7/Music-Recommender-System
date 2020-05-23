6e8dbc5078ff8d944cd55a4e461d131eb5d8fc4d
Vimal Subbiah <45879512+vimalsubbiah7@users.noreply.github.com>
2020-05-24 02:55:18 +0530

    initial commit

---
+++ Model.py
@@ -0,0 +1,36 @@
+# MUSIC RECOMMENDER SYSTEM
+
+import numpy as np
+import pandas
+from sklearn.model_selection import train_test_split
+import  time
+from sklearn.externals import joblib
+
+triplets_file = '10000.txt'
+songs_metadata_file = 'song_data.csv'
+
+song_df_1 = pandas.read_table(triplets_file, header=None)
+song_df_1.columns = ['user_id', 'song_id', 'listen_count']
+song_df_2 = pandas.read_csv(songs_metadata_file)
+song_df = pandas.merge(song_df_1, song_df_2.drop_duplicates(['song_id']), on="song_id", how="left")
+#print(song_df.head())
+song_df = song_df.head(10000)
+song_df['song'] = song_df['title'].map(str) + " - " + song_df['artist_name']
+
+song_grouped = song_df.groupby(['song']).agg({'listen_count': 'count'}).reset_index()
+grouped_sum = song_grouped['listen_count'].sum()
+song_grouped['percentage']  = song_grouped['listen_count'].div(grouped_sum)*100
+song_grouped.sort_values(['listen_count', 'song'], ascending = [0,1])
+#print(song_grouped.head())
+
+users = song_df['user_id'].unique()
+#print(len(users))
+songs = song_df['song'].unique()
+#print(len(songs))
+
+#CREATE A RECOMMENDER
+train_data, test_data = train_test_split(song_df, test_size = 0.20, random_state=0)
+#print(train_data.head(5))
+
+
+
